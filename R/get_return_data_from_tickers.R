254c9241c99fa3588baaaa7703ea4bb6eff7810a
rubetron <alexandre.rubesam@gmail.com>
2022-04-01 09:33:20 +0200

    get_return_data_from_tickers now considers only weekdays and uses na.locf to avoid NAs added .git to .Rbuildignore

--- R/get_return_data_from_tickers.R
+++ R/get_return_data_from_tickers.R
@@ -1,7 +1,7 @@
 #' @export
 #' @importFrom quantmod getSymbols
 #' @importFrom PerformanceAnalytics CalculateReturns
-#' @importFrom zoo index
+#' @importFrom zoo index na.locf
 #' @import xts
 get_return_data_from_tickers <- function(tickers, starting_date = "2007-01-01"){
   getSymbols(tickers, from = starting_date, source = 'yahoo')
@@ -12,6 +12,17 @@ get_return_data_from_tickers <- function(tickers, starting_date = "2007-01-01"){
     prices  <- merge.xts(prices, get(tickers[i])[,6])
   }
   colnames(prices) <- tickers
+
+  # consider only weekdays: makes it possible
+  # to mix exchange-traded and crypto
+  w_days <- weekdays(index(prices))
+  w_ends <- which(w_days == "Saturday" | (w_days == "Sunday"))
+  if (length(w_ends) > 0){
+    prices <- prices[-w_ends, ]
+  }
+
+  # take care of missing prices to avoid NA returns
+  prices <- na.locf(prices)
 
   #calculate returns
   returns <- CalculateReturns(prices)
